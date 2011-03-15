1a4749ff0ba70244c600522f3b19745f42cce901
danbob <danbob@gmail.com>
2011-03-15 19:07:47 -0400

    Committing my warmup project from way back when, to be used as a project template.

---
+++ BumpMaps.h
@@ -0,0 +1,54 @@
+// Geometric Tools, LLC
+// Copyright (c) 1998-2010
+// Distributed under the Boost Software License, Version 1.0.
+// http://www.boost.org/LICENSE_1_0.txt
+// http://www.geometrictools.com/License/Boost/LICENSE_1_0.txt
+//
+// File Version: 5.0.0 (2010/01/01)
+
+#ifndef BUMPMAPS_H
+#define BUMPMAPS_H
+
+#include "Wm5WindowApplication3.h"
+#include "SimpleBumpMapEffect.h"
+using namespace Wm5;
+
+class BumpMaps : public WindowApplication3
+{
+    WM5_DECLARE_INITIALIZE;
+    WM5_DECLARE_TERMINATE;
+
+public:
+    BumpMaps ();
+
+    virtual bool OnInitialize ();
+    virtual void OnTerminate ();
+    virtual void OnIdle ();
+    virtual bool OnKeyDown (unsigned char key, int x, int y);
+	virtual bool OnMouseClick (int button, int state, int x, int y, unsigned int modifiers);
+
+protected:
+    void CreateScene ();
+    TriMesh* CreateTorus ();
+    TriMesh* CreateSquare ();
+	TriMesh* CreateCylinder ();
+	TriMesh* CreateSphere ();
+	TriMesh* CreateCube ();
+    void UpdateBumpMap ();
+
+    NodePtr mScene;
+    Culler mCuller;
+    AVector mLightDirection;
+	APoint mLightPosition;
+    bool mUseTorus;
+    bool mUseBumpMap;
+    Float4 mTextColor;
+
+	Picker mPicker;
+	char mPickMessage[1024];
+};
+
+WM5_REGISTER_INITIALIZE(BumpMaps);
+WM5_REGISTER_TERMINATE(BumpMaps);
+
+#endif
