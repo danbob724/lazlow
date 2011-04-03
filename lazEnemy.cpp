cba68b6a14b76c78965a33544b3bafa8a6b4891a
Adam <ambartos@gmail.com>
2011-04-03 01:07:01 -0400

    Added lazEnemy.cpp and lazEnemy.h, currently not functional (basically just copied projectile code for now).

---
+++ lazEnemy.cpp
@@ -0,0 +1,27 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * lazlow
+ * 
+ */
+
+#include "lazEnemy.h"
+
+lazEnemy::lazEnemy() {};
+
+lazEnemy::lazEnemy(ShapeMaker* someShapeMaker) {
+	myShapeMaker = someShapeMaker;
+	mesh = myShapeMaker->CreateSphere();
+
+	loc = APoint::ORIGIN; 
+	x_dir = 0;
+	z_dir = 0;
+	
+	mesh->LocalTransform.SetScale(APoint(0.35f, 0.35f, 0.35f));
+	mesh->LocalTransform.SetTranslate(APoint(0.0f, 100.0f, 0.0f));
+}
+
+void lazEnemy::Update() {
+	//do the movement updating
+	loc = loc + AVector(x_dir, 0.0, z_dir);
+	mesh->LocalTransform.SetTranslate(APoint(loc));
+}
---
+++ lazEnemy.h
@@ -0,0 +1,35 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * lazlow
+ * 
+ */
+
+#ifndef _LAZENEMY_H_
+#define _LAZENEMY_H_
+
+#include "Wm5TriMesh.h"
+#include "shapeMaker.h"
+
+#define NUM_ENEMIES 5
+
+using namespace Wm5;
+
+class lazEnemy
+{
+	public:
+		lazEnemy();
+		lazEnemy(ShapeMaker* someShapeMaker);
+		TriMesh* mesh;
+		APoint loc;
+		float x_dir, z_dir;
+		void Update();
+		
+	protected:
+		ShapeMaker* myShapeMaker;
+		Environment* enemyEnv;
+
+	private:
+
+};
+
+#endif // _LAZENEMY_H_
