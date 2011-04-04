a4f4a09238d8a8d0909ae4c91882f83553d78d3e
Adam <ambartos@gmail.com>
2011-04-04 00:59:14 -0400

    Made more generic enemy collsion test and movement methods.

---
+++ playerCharacter.cpp
@@ -0,0 +1,31 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * lazlow
+ * 
+ */
+
+#include "playerCharacter.h"
+#include "Wm5Environment.h"
+
+PlayerCharacter::PlayerCharacter() {};
+
+PlayerCharacter::PlayerCharacter(ShapeMaker* someShapeMaker) {
+	curSummon = 0;
+	mShapeMaker = someShapeMaker;
+	curLocation = APoint(0.0f, 0.0f, 0.0f);
+	mMesh = someShapeMaker->CreateTorus();
+}
+
+void PlayerCharacter::setLocation(APoint desiredLocation) {
+	curLocation = desiredLocation;
+	mMesh->LocalTransform.SetTranslate(curLocation);
+}
+
+void PlayerCharacter::movePlayer(AVector moveVector) {
+	curLocation = curLocation + moveVector;
+	mMesh->LocalTransform.SetTranslate(curLocation);
+}
+
+APoint PlayerCharacter::getLocation() {
+	return curLocation;
+}
---
+++ playerCharacter.h
@@ -0,0 +1,39 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * lazlow
+ * 
+ */
+
+#ifndef _PLAYERCHARACTER_H_
+#define _PLAYERCHARACTER_H_
+
+#include "shapeMaker.h"
+#include "Wm5TriMesh.h"
+#include "Wm5APoint.h"
+#include "Wm5AVector.h"
+
+using namespace Wm5;
+
+class PlayerCharacter
+{
+	public:
+		PlayerCharacter();
+		PlayerCharacter(ShapeMaker* someShapeMaker);
+		void setLocation(APoint desiredLocation);
+		TriMesh* mMesh;
+		APoint getLocation();
+		void movePlayer(AVector moveVector);
+
+		float radius;
+
+	protected:
+		ShapeMaker* mShapeMaker;
+		unsigned char curSummon;
+		APoint curLocation;
+		Environment* playerCharEnv;
+
+	private:
+		
+};
+
+#endif // _PLAYERCHARACTER_H_
