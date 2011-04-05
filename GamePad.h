1536d4234820f4147907adca76353a9ed08772db
danbob <danbob@gmail.com>
2011-04-04 23:15:34 -0400

    Gamepad?

--- GamePad.h
+++ GamePad.h
@@ -3,30 +3,32 @@
 
 #include "SDL.h"
 #include "Wm5AVector.h"
+using namespace Wm5;
 
 class GamePad
 {
 
 public:
 
-	GamePad(void);
+        GamePad(void);
 
-	//Initializes SDL and connects joystick
-	bool initialize();
+        //Initializes SDL and connects joystick
+        bool initialize();
 
-	//Check if gamepads are enabled and connected.
-	bool isEnabled();
-	
-	//Closes the joystick and quits SDL.
-	bool closeStick();
+        //Check if gamepads are enabled and connected.
+        bool isEnabled();
+        
+        //Closes the joystick and quits SDL.
+        void closeStick();
 
-	//Polls the joystick, updating leftStick and rightStick,
-	//then shooting/moving as necessary.
-	void poll();
+        //Polls the joystick, updating leftStick and rightStick,
+        //then shooting/moving as necessary.
+        void poll();
 
-	AVector leftStick;
-	AVector rightStick;
+        AVector leftStick;
+        AVector rightStick;
 
 };
 
 #endif
+
