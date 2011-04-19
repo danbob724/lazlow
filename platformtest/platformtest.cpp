0edea70bfa0eb2b9539e9d83aeaa7c232aaa20c6
danbob <danbob@gmail.com>
2011-04-18 23:48:59 -0400

    Eliminated default WildMagic5 keybindings.

---
+++ platformtest/platformtest.cpp
@@ -0,0 +1,19 @@
+#include <time.h>
+#include <iostream>
+
+//this nonsense is to get the current working directory
+#include <stdio.h>
+#ifdef WIN32
+    #include <direct.h>
+    #define GetCurrentDir _getcwd
+#else
+    #include <unistd.h>
+    #define GetCurrentDir getcwd
+#endif
+
+int main() {
+	std::cout << "Clocks per second:\t" << CLOCKS_PER_SEC << std::endl;
+	clock_t clock0;
+	clock_t clock1;
+	return 0;
+}
